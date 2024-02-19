7ec97384f12816c957673aeac3785554dd430568
Aakash Bhardwaj <51430279+aakash-bhardwaj-1@users.noreply.github.com>
2024-02-19 19:59:13 +0530

    Update PatientDetails.java

--- src/main/java/com/example/demo/Entity/patient/PatientDetails.java
+++ src/main/java/com/example/demo/Entity/patient/PatientDetails.java
@@ -5,45 +5,6 @@ import lombok.*;
 
 import java.util.Date;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString
-@Entity
-@Table(name="patient_details")
+
 public class PatientDetails {
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Id
-    private int pid;
-
-    @Column(nullable = false)
-    private int abha_id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = true)
-    private int mobile_no;
-
-    @Column(nullable = false)
-    private Date dob;
-
-    @Column(nullable = false)
-    private String preferred_language;
-
-    @Column(nullable = false)
-    private String gender;
-
-    @Column(nullable = false)
-    private String Address;
-
-    @Column(nullable = false)
-    private int pinecode;
-
-    @Column(nullable = false)
-    private int blockcode;
-
-
 }
